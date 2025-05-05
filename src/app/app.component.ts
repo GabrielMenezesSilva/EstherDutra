21a08fa10096724d1b6e4be519f3dd48cedd9a63
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2025-05-05 15:29:25 +0200

    Adiciona configuração do Firebase, arquivos de hospedagem e regras de armazenamento, além de atualizar dependências do Angular e Firebase. Inclui páginas HTML de erro 404 e de boas-vindas, e refatora componentes para melhor organização e uso de módulos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
